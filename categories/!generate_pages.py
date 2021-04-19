9b710d4715b23f604fb59c7dc5b4d8ff957b53f0
Ivan Shtarev <ishtarev@outlook.com>
2021-04-19 19:05:02 +0500

    Created site

---
+++ categories/!generate_pages.py
@@ -0,0 +1,16 @@
+array = ['police',
+        'ninetys',
+        'not_grandma',
+        'new_russians',
+        'sherlock',
+        'relationship',
+        'animals',
+        'breznev',
+        'religion',
+        'ussr',
+        'putin',
+        'obama',
+        'diet',
+        'hitler']
+for i in array:
+    open(i + '.html', 'w')
